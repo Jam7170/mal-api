90fcd7533ea57598574ee21718a0d41e1ecb3207
= <=>
2022-12-25 12:51:18 +0000

    First Commit

---
+++ malapi.py
@@ -0,0 +1,26 @@
+import os, requests, json
+from typing import Optional
+
+BASE_URL = "https://api.myanimelist.net/v2"
+
+def load_secrets(path_):
+    if not os.path.exists(path_): return
+    with open(path_, 'r') as f:
+        secrets = json.loads(f.read())
+    return secrets['client-id'], secrets['client-secret']
+
+class MAL:
+    def __init__(self, client_id: str, client_secret: Optional[str] = None):
+        self._client_id = client_id
+
+    def _get_without_auth(self, endpoint):
+        url = f"{BASE_URL}/{endpoint}"
+        res = requests.get(url,headers={"X-MAL-CLIENT-ID": self._client_id})
+        return res
+
+    def get_anime_by_id(self, anime_id: int | str) -> dict:
+        return self._get_without_auth(f'anime/{anime_id}').json()
+
+    def search_anime(self, query: str, limit: int=20) -> dict:
+        endpoint='anime?q={query}&limit={limit}'
+        return self._get_without_auth(endpoint).json()
